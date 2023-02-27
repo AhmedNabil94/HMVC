50984957b1b2cae25f5d73b160513f6d519854e3
AhmedNabil94 <mody998866@gmail.com>
2023-02-27 20:06:59 +0200

    hidding unnecessary checks

--- app/Modules/User/routes.php
+++ app/Modules/User/routes.php
@@ -1,5 +1,5 @@
 <?php
 
     Route::get('/',[\App\Http\Controllers\UserController::class,'index']);
-    Route::get('/create',[\App\Http\Controllers\UserController::class,'create']);
-    Route::get('/update',[\App\Http\Controllers\UserController::class,'update']);
+//    Route::post('/create',[\App\Http\Controllers\UserController::class,'create']);
+//    Route::post('/update',[\App\Http\Controllers\UserController::class,'update']);
